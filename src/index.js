07e12f3ab8c6a200e364bbbf994dbaaab52e6173
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-20 13:47:55 -0500

    added the climaceil api for request of the city

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
-const wKey = 'UJkWsapTKh6XOfJS2Ru41FaJxw91Rs6Q';
-const appID = 'plJANF80UWQC';
-const appKey = '1d63e8697da40ad9323a23250c18de76';
-import places from 'places.js';
+const wKey = "UJkWsapTKh6XOfJS2Ru41FaJxw91Rs6Q";
+const appID = "plJANF80UWQC";
+const appKey = "1d63e8697da40ad9323a23250c18de76";
+const latitude = 19.5408;
+const longitude = -96.9146;
+// ["temp", "feels_like", "dewpoint", "humidity", wind_speed, sunrise, sunset, weather_code]
+import places from "places.js";
+const query = (lat, lon, apiKey) => {
+  return `https://api.climacell.co/v3/weather/realtime?apikey=${apiKey}&lat=${lat}&lon=${lon}&unit_system=si&fields=temp,feels_like`;
+};
+let lat;
+let long;
 
 (function () {
   const placesAutocomplete = places({
     appId: appID,
     apiKey: appKey,
-    container: document.querySelector('#city'),
+    container: document.querySelector("#city"),
   });
 
-  const city = document.querySelector('#city-value');
+  const city = document.querySelector("#city-value");
 
-  placesAutocomplete.on('change', (e) => {
+  placesAutocomplete.on("change", (e) => {
     city.textContent = e.suggestion.value;
-    console.log(e);
+
+    lat = e.suggestion.latlng.lat;
+    long = e.suggestion.latlng.lng;
+    console.log(lat, long);
+    fetch(query(lat, long, wKey), { mode: "cors" })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
   });
 
-  placesAutocomplete.on('clear', () => {
-    city.textContent = 'none';
+  placesAutocomplete.on("clear", () => {
+    city.textContent = "none";
   });
-}());
+})();
