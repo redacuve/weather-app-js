3459f6e169873dd860941bf89d7ec8d24ee07a13
Rey David Cuevas <cuevas.vela@gmail.com>
2020-06-20 17:03:40 -0500

    added basic structure to the html file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
+import "./style.css"
+import places from "places.js";
+
 const wKey = "UJkWsapTKh6XOfJS2Ru41FaJxw91Rs6Q";
 const appID = "plJANF80UWQC";
 const appKey = "1d63e8697da40ad9323a23250c18de76";
-const latitude = 19.5408;
-const longitude = -96.9146;
-// ["temp", "feels_like", "dewpoint", "humidity", wind_speed, sunrise, sunset, weather_code]
-import places from "places.js";
+let lat;
+let long;
+
+
 const query = (lat, lon, apiKey) => {
   return `https://api.climacell.co/v3/weather/realtime?apikey=${apiKey}&lat=${lat}&lon=${lon}&unit_system=si&fields=temp,feels_like`;
 };
-let lat;
-let long;
+
 
 (function () {
   const placesAutocomplete = places({
