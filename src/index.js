ce3ccb7c50b536ba30ca5cfdd1cc4c36d6cd4d49
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-27 15:17:21 -0500

    removed algolia search

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import './style.css';
-import places from 'places.js';
 import changeUnits from './changeUnits';
 import { addDeleteEvent, openNotification } from './notificationHandler';
 
 const wKey = 'UJkWsapTKh6XOfJS2Ru41FaJxw91Rs6Q';
-const appID = 'plJANF80UWQC';
-const appKey = '1d63e8697da40ad9323a23250c18de76';
 const query = (lat, lon, apiKey, unit) => `https://api.climacell.co/v3/weather/realtime?apikey=${apiKey}&lat=${lat}&lon=${lon}&unit_system=${unit}&fields=temp,feels_like,precipitation,wind_speed,visibility,weather_code`;
 const city = document.querySelector('#city-name');
 let lat;
 let long;
 
+const citySearch =  document.getElementById('city');
+let cities;
+window.onload = async () => {
+  const resp = await fetch('./JSON/city.list.json');
+  cities = await resp.json();
+  document.querySelector('.spinner').classList.add('hide');
+  citySearch.classList.remove('hide');
+}
+
+
+
 document.getElementById('units-toggler').addEventListener('change', changeUnits);
 addDeleteEvent();
+
+async function searchCity(query) {
+  console.log(query);
+  console.log(cities);
+}
+citySearch.addEventListener('input', () => searchCity(citySearch.value));
+
 
 const placesAutocomplete = places({
   appId: appID,
